f00a973368d531b7f934362eb47ce8a37df67894
erikfora11 <foreroeriksantiago8@gmail.com>
2022-10-09 12:58:36 -0500

    tweaks

--- src/main/java/erik/main/Reto3FinalApplication.java
+++ src/main/java/erik/main/Reto3FinalApplication.java
@@ -2,8 +2,9 @@ package erik.main;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class Reto3FinalApplication {
 
     public static void main(String[] args) {
